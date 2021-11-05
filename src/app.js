99494a39451403f9c6cfb298242a32cc574bd8dc
Angelo Geant Gaviola <yomama@example.com>
2021-11-05 13:49:19 +0800

    added API_VERSION as a environment variable

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const PORT = 8080 || process.env.PORT;
 const app = express();
 require("dotenv").config();
+const API_VERSION = process.env.API_VERSION;
 
 const newsRoute = require("./routes/news");
 
-app.use("/news", newsRoute);
+app.use(`/${API_VERSION}/news`, newsRoute);
 
 app.get("/", (req, res) => {
   res.send("hello");
