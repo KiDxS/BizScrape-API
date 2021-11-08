5f06ea6848ac1f4b1b49a14b87c081003448591a
Angelo Geant Gaviola <yomama@example.com>
2021-11-08 11:33:30 +0800

    added the crawlControllerMiddleware to execute in the application-level

--- src/app.js
+++ src/app.js
@@ -3,16 +3,20 @@ const PORT = 8080 || process.env.PORT;
 const app = express();
 require("dotenv").config();
 const API_VERSION = process.env.API_VERSION;
+const crawlerControlMiddleware = require("./middleware/crawlerControl");
 
 const newsRoute = require("./routes/news");
 
+// Executes the crawler every 2 minutes
+crawlerControlMiddleware();
+
 app.use(`/${API_VERSION}/news`, newsRoute);
+
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
