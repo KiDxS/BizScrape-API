2528546db6964f3f233e3fbc198192370889d04c
Angelo Geant Gaviola <yomama@example.com>
2021-11-16 10:09:12 +0800

    added a way to filter which source you want to pull data from

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -1,17 +1,26 @@
 const crawler = require("../utils/crawl");
 
 const main = async (req, res, next) => {
-  try {
-    const source = req.query.source;
-    if (source === undefined) {
-      const results = await crawler.results;
-      return res.status(200).json(results);
-    } else {
-      console.log(source);
+    try {
+        const source = req.query.source;
+        const results = await crawler.results;
+        if (source === undefined) {
+            return res.status(200).json(results);
+        } else {
+            const filteredResults = [];
+            for (const index in results) {
+                const element = results[index];
+                const retrieveSource = results[index].source;
+
+                if (retrieveSource == source) {
+                    filteredResults.push(element);
+                }
+            }
+            return res.status(200).json(filteredResults);
+        }
+    } catch (err) {
+        console.log(err);
     }
-  } catch (err) {
-    console.log(err);
-  }
 };
 
 module.exports = main;
