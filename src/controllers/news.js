4956acfcfcd80d52c491ddfe7229683a695fd981
Angelo Geant Gaviola <yomama@example.com>
2021-11-18 14:02:08 +0800

    added comments for easier readability

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -4,19 +4,25 @@ const main = async (req, res, next) => {
     try {
         const { source } = req.query;
         const results = await crawler.results;
+        // If the source query parameter is null:
         if (source === undefined) {
             return res.status(200).json({
                 success: true,
                 message: "The data has been pulled",
                 data: results,
             });
-        } else {
+        }
+        // If the source query parameter is not null:
+        else {
             const filteredResults = [];
+            // Loops through the array
             for (const index in results) {
                 const element = results[index];
+                // Declares retrieveSource to be a variable for accessing the source property of the elements.
                 const retrieveSource = results[index].source;
-
+                // Compares whether the value of the source property is the same with the source query parameter.
                 if (retrieveSource == source) {
+                    // Pushes the element to the filteredResults array.
                     filteredResults.push(element);
                 }
             }
