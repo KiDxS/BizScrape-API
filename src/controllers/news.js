e2f5a719b8dbad233dca2b733dd4939751a726b3
Angelo Geant Gaviola <yomama@example.com>
2021-11-18 15:38:08 +0800

    changed to using route parameters

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -2,8 +2,35 @@ const crawler = require("../utils/crawl");
 
 const main = async (req, res, next) => {
     try {
-        const { source } = req.query;
+        const { source } = req.params;
         const results = await crawler.results;
+
+        if (source) {
+            const filteredResults = [];
+            // Loops through the array
+            for (const index in results) {
+                const element = results[index];
+                // Declares retrieveSource to be a variable for accessing the source property of the elements.
+                const retrieveSource = results[index].source;
+                // Compares whether the value of the source property is the same with the source query parameter.
+                if (retrieveSource == source) {
+                    // Pushes the element to the filteredResults array.
+                    filteredResults.push(element);
+                }
+            }
+            return res.status(200).json({
+                success: true,
+                message: "The data has been pulled",
+                data: filteredResults,
+            });
+        } else {
+            return res.status(200).json({
+                success: true,
+                message: "The data has been pulled",
+                data: results,
+            });
+        }
+        /*
         // If the source query parameter is null:
         if (source === undefined) {
             return res.status(200).json({
@@ -32,6 +59,7 @@ const main = async (req, res, next) => {
                 data: filteredResults,
             });
         }
+        */
     } catch (err) {
         console.log(err);
     }
