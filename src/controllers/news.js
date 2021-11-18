adaf55c45b194734c8594ed5e900c6377f7f738d
Angelo Geant Gaviola <yomama@example.com>
2021-11-18 18:16:56 +0800

    added comments in the news.js

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -4,7 +4,7 @@ const main = async (req, res, next) => {
     try {
         const { source } = req.params;
         const results = await crawler.results;
-
+        // If source is not undefined or null
         if (source) {
             const filteredResults = [];
             // Loops through the array
@@ -23,43 +23,15 @@ const main = async (req, res, next) => {
                 message: "The data has been pulled",
                 data: filteredResults,
             });
-        } else {
-            return res.status(200).json({
-                success: true,
-                message: "The data has been pulled",
-                data: results,
-            });
-        }
-        /*
-        // If the source query parameter is null:
-        if (source === undefined) {
-            return res.status(200).json({
-                success: true,
-                message: "The data has been pulled",
-                data: results,
-            });
-        }
-        // If the source query parameter is not null:
+        } 
+        // If source is undefined or null
         else {
-            const filteredResults = [];
-            // Loops through the array
-            for (const index in results) {
-                const element = results[index];
-                // Declares retrieveSource to be a variable for accessing the source property of the elements.
-                const retrieveSource = results[index].source;
-                // Compares whether the value of the source property is the same with the source query parameter.
-                if (retrieveSource == source) {
-                    // Pushes the element to the filteredResults array.
-                    filteredResults.push(element);
-                }
-            }
             return res.status(200).json({
                 success: true,
                 message: "The data has been pulled",
-                data: filteredResults,
+                data: results,
             });
         }
-        */
     } catch (err) {
         console.log(err);
     }
