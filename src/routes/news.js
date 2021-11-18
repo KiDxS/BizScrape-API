939db15852f53428a0603dec48fda0a44bae5802
Angelo Geant Gaviola <yomama@example.com>
2021-11-18 15:39:59 +0800

    added a source route parameter

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const Router = express.Router();
 const newsController = require("../controllers/news");
+const { validate, validateSource } = require("../middleware/validator");
 
-Router.get("/", newsController);
+Router.get("/", validateSource(), validate, newsController);
+
+Router.get("/:source", newsController);
 
 module.exports = Router;
