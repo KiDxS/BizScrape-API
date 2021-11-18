7600cb3164a16c0705c106b9422b3731eb853346
Angelo Geant Gaviola <yomama@example.com>
2021-11-18 18:18:29 +0800

    added comments in validator.js

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,8 +1,9 @@
 const { param, validationResult } = require("express-validator");
 
 const validateSource = () => {
-    // The list of sources
+    // The list of sources; we are putting undefined to the array so that users can 
     const sources = ["manilabulletin", "businessmirror"]
+    // Returns an array of validation that we will do.
     return [
         param("source").isIn(sources).withMessage({
             success: false,
@@ -18,6 +19,7 @@ const validate = (req, res, next) => {
         return next();
     }
     const extractedErrors = [];
+    // Pushes the errors to a new array
     errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
     return res.status(400).json(extractedErrors);
 };
