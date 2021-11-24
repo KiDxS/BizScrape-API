4225e86380ae9ecde8c1138114ad5a07e0ce9ca6
Angelo Geant Gaviola <yomama@example.com>
2021-11-24 10:44:21 +0800

    updated crawlerControl

--- src/utils/crawlerControl.js
+++ src/utils/crawlerControl.js
@@ -1,7 +1,7 @@
 const crawler = require("./crawl");
 
 // Milliseconds of 12 hours
-const milliseconds = 12000 * 60 * 60;
+const milliseconds = 12000 * 3600;
 
 const main = async () => {
     setInterval(async () => {
